a3fd2ba4d4221ebd57b060d9bcaf686bc18cb9d6
Junghoon Lee <junghoon@picwell.com>
2017-12-13 00:47:01 -0500

    A simple notebook to test drive the client code.

---
+++ test_lambda_client.py
@@ -0,0 +1,57 @@
+
+# coding: utf-8
+
+# In[1]:
+
+get_ipython().magic(u'load_ext autotime')
+
+
+# In[2]:
+
+import json
+import time
+
+from etltools import s3
+from test_client import LambdaCalculatorTestClient
+
+
+# In[3]:
+
+client = LambdaCalculatorTestClient({'profile_name': 'sandbox'})
+
+
+# # Test Cost Breakdown
+
+# In[4]:
+
+uid = '1000101'
+pids = ['2820028008119', '2820088001036']
+
+client.calculate_breakdown(uid, pids)
+
+
+# # Test Batch Calculation
+
+# In[5]:
+
+uids = s3.read_json('s3n://picwell.sandbox.medicare/samples/philadelphia-2015')
+
+print '{} uids read'.format(len(uids))
+
+
+# In[6]:
+
+uids[:10]
+
+
+# In[ ]:
+
+for uid in uids:
+    client.calculate_async(uid, months=['01'])
+    time.sleep(1.0/40)  # 40 requests for second
+
+
+# In[ ]:
+
+
+
